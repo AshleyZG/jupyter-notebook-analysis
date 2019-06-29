cb17ccb86c019867bfacb0018320a5bfdf91cbb9
AshleyZG <gsarazh@gmail.com>
2019-06-29 22:16:44 +0800

    config & constants

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
 # nb_path = '/data/nfsdata/zhangge/data/sample_data/data/codes'
 # nb_path = '/data/nfsdata/zhangge/data/notebooks_1/codes'
-nb_path = '/Users/zhangge/Desktop/codes'
+nb_path = '/home/gezhang/data/jupyter/data_science_py'
+toy_path = '/home/gezhang/data/jupyter/toy'
+key_methods_path = './key_methods.json'
 # nb_path = '/Users/zhangge/NLP/UW/data/notebooks'
 import_path = '/data/nfsdata/zhangge/data/sample_data/data/import_nbs'
 libraries = ['numpy', 'scipy', 'pandas', 'matplotlib', 'sklearn', 'plotly']
 
+KEY_TOKEN_PATH = './key_tokens.txt'
+# PY_PATH = '/home/gezhang/data/jupyter/py'
+funcs_path = './funcs.txt' # 从代码中抽取的所有函数语料
+raw_graph_path = './graphs_new'
+toy_raw_graph_path = './toy_graphs_new'
+key_func_path = './key_func.json' # word2vec 得到的 key_func
+func_counter_path = './func_counter.json'
+target_vocab_path='./target_vocab.json'
