0458abc1c0a112926832a441c19a8c2ef1d93bd8
AshleyZG <gsarazh@gmail.com>
2019-07-01 08:16:10 +0800

    word2vec on function tokens

--- word2vec.py
+++ word2vec.py
@@ -1,19 +1,17 @@
+'compute word2vec model based on certain corpus'
 import gensim
 import os
 from gensim.models import word2vec
+import pdb
+import time
+import json
+import itertools
+import re
 
-sentences = word2vec.Text8Corpus(
-    '/home/zhangge/jupyter-notebook-analysis/python2.txt')
-model = word2vec.Word2Vec(sentences, window=16, size=200, min_count=4,)
-
-# print(model.similarity('dfx.to_csv', 'df.to_csv'))
-# print(model.similarity('乔峰', '慕容复'))
-print(model.most_similar("sklearn.metrics.r2_score"))
-print(model.most_similar("sklearn.lda.LDA"))
-print(model.most_similar("sklearn.naive_bayes.MultinomialNB.fit"))
-# print(model.most_similar("sklearn.decoposition.PCA"))
-print(model.most_similar("sklearn.metrics.accuracy_score"))
-# print(model.most_similar("scipy.signal.medfilt", topn=20))
-# print(model.most_similar(u"softmax"))
-# print(model.most_similar(u"xgb_model.predict"))
-# model.most_similar(positive=['woman', 'king'], negative=['man'], topn=1)
+func_token_path = '/home/gezhang/jupyter-notebook-analysis/func_tokens.txt'
+corpus = word2vec.Text8Corpus(func_token_path)
+start_time = time.time()
+model = word2vec.Word2Vec(corpus, window=10, size=512, min_count=4,)
+print('[Info] train model: {} s'.format(time.time() - start_time))
+model.save("word2vec.model")
+# pdb.set_trace()
